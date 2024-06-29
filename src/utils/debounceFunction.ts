a4a7da2e3e412960c2d282c9c5156fce1c6fe7ac
Arvin-Hacks <boatgamer0980@gmail.com>
2024-06-29 15:28:09 +0530

    Typscript config changed and debounce fixed

--- src/utils/debounceFunction.ts
+++ src/utils/debounceFunction.ts
@@ -1,11 +1,9 @@
-// src/utils/debounce.ts
 export function debounce<T extends (...args: any[]) => void>(func: T, delay: number): (...args: Parameters<T>) => void {
-    let timeoutId: ReturnType<typeof setTimeout>;
-    return function (...args: Parameters<T>) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => {
-        func.apply(this, args);
+          func.apply(this, args);
       }, delay);
-    };
-  }
-  
+  };
+}
