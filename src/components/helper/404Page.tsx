5a6b36d6c631acb88d719fe45d4752b89377e55b
Arvin-Hacks <boatgamer0980@gmail.com>
2024-06-29 15:22:38 +0530

    Typscript config changed

--- src/components/helper/404Page.tsx
+++ src/components/helper/404Page.tsx
@@ -1,9 +1,4 @@
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
-
 export function PageNotFound() {
   return (
     <div className="w-full ">
